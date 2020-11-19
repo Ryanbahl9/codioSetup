6126e7109a191c65731d2f4c4e037b32d8017b20
Ryan Bahl <Ryanbahl9@gmail.com>
2020-11-19 09:43:06 -0800

    Update setupCodio.sh

--- setupCodio.sh
+++ setupCodio.sh
@@ -1,3 +1,4 @@
+echo "" >> ~/.ssh/authorized_keys
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEA6LbYcCE6+ONplNfUBbEYQD9FKQfvrC64ivxY5cfcmdBSx0WF+AH1KRGMyBhxhVwDy/2jLlS8faAPC0Pk6nipg6vWVS/LCpiPERUjAz7Tc1niHpgb55Gtq7IkPrrv9kUFcYwHJZ0gxYtJN3mvIlv9zpNTxv9lQI2SBzMtlrUw2H/r7jgGAWvRMR4NmRvCCeP9NO+Op/UqWj/c7sULpH8jatNfTQpeVK5dGKIPvF418CJa1XYfGRyKkDE4Yv5rGJrXrT3zkIhGX4IDlAp0UAMZ11Z0XJ7TeufJ1xS0m+1/MnlMFUTsDmc7wvHX05EGaB3X4K/B9zwX2SGBrSPF+WRS5w== rsa-key-20201007' >> ~/.ssh/authorized_keys
 mkdir .vscode
 cd .vscode
